645718ef168efa4c7411c29aa6f5910a784298eb
daiki <dik.piyo.piyo@gmail.com>
2019-08-15 14:41:43 +0900

    add radio button

--- src/js/index.js
+++ src/js/index.js
@@ -12,38 +12,59 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
   accessToken: process.env.MAPBOX_ACCESS_TOKEN
 }).addTo(mymap)
 
-// L.marker([35.589521, 139.591704]).addTo(mymap)
-//   .bindPopup('test')
-//   .openPopup()
-
+// 飛行計画
+const flightPlan = []
+// チェックポイントid
+let checkPointID = 0
+// チェックポイントを通る時間
+let time = 0
 // 追加したマーカーの緯度経度リスト
 const addedMarkerList = []
 
+// ラジオボタンでモード切り替え
+let mode = document.getElementById('modeSelect').mode.value
+document.getElementById('modeSelect').addEventListener('change', (e) => {
+  mode = e.target.value
+}, false)
+
+// チェックポイントを飛行計画に追加
+const addCheckPointToFlightPlan = (time, id) => {
+  flightPlan.push({time: time, checkPointID: id})
+}
+
 // マーカーの追加
-const addMarker = (lat, lng) => {
-  L.marker([lat, lng]).addTo(mymap)
+const addMarker = (lat, lng, id) => {
+  L.marker([lat, lng], {checkPointID: id}).addTo(mymap)
+    .on('click', (e) => {
+      if (mode === 'makeFlightPlan') {
+        addCheckPointToFlightPlan(time, e.target.options.checkPointID)
+        time++
+        console.log(flightPlan)
+      }
+    })
 }
 
 // クリック時の処理
-const onMapClick = (e) => {
-  addMarker(e.latlng.lat, e.latlng.lng)
-  addedMarkerList.push({lat: e.latlng.lat, lng: e.latlng.lng})
-  console.log('add marker to (' + e.latlng.lat + ',' + e.latlng.lng + ',' + 0 + ')')
+const handleMapClick = (e) => {
+  if (mode === 'addCheckPoint') {
+    addMarker(e.latlng.lat, e.latlng.lng, checkPointID)
+    checkPointID++
+    addedMarkerList.push({lat: e.latlng.lat, lng: e.latlng.lng})
+    console.log('add marker to (' + e.latlng.lat + ',' + e.latlng.lng + ',' + 0 + ')')
+  }
 }
 
 // クリックイベントの追加
-mymap.on('click', onMapClick)
+mymap.on('click', handleMapClick)
 
 // ファイル選択時の処理
 const handleFileSelect = (e) => {
-  console.log(e)
   const file = e.target.files[0]
   if (file.name !== 'checkpoint.csv') {
     return
   }
   const reader = new FileReader()
   reader.onload = () => {
-    console.log(reader.result)
     // 改行ごとに分割
     const checkPointInput = reader.result.split('\n')
     // ヘッダ部分を削除
@@ -55,10 +76,10 @@ const handleFileSelect = (e) => {
                                           .map(x => {
                                             return {lat: x[0], lng: x[1], alt: x[2]}
                                           })
-    console.log(checkPointList)
     // csvファイルから読み取ったチェックポイントを出力
     for (let checkPoint of checkPointList) {
-      addMarker(checkPoint.lat, checkPoint.lng)
+      addMarker(checkPoint.lat, checkPoint.lng, checkPointID)
+      checkPointID++
     }
   }
   reader.readAsText(file)
